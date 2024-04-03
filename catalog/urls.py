64c7227c003f46063774eb3ead5e2625cc2dc918
Luis Aguila <aguilaluis22@gmail.com>
2024-04-02 22:00:25 -0600

    Configuración de archivos html, statics, creación de menu y estilación inicial de la página

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name="index"),
+    path('consultas/', views.consultas, name="consultas"),
+    path('capturas/', views.capturas, name="capturas")
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from django.urls import include
+from django.urls import include, path
 
 # Create your views here.
 def index(request):
-    return render(request, include('index.html'))
+    return render(request, 'index.html')
+
+def consultas(request):
+    return render(request, 'consultas.html')
+
+def capturas(request):
+    return render(request, 'capturas.html')
