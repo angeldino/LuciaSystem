826efd6ec0fcf260f150566f89d9f6f90cc4c995
Luis Aguila <aguilaluis22@gmail.com>
2024-04-04 17:04:40 -0600

    Add files via upload

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import *
 from django.urls import include, path
 
 # Create your views here.
@@ -6,7 +7,8 @@ def index(request):
     return render(request, 'index.html')
 
 def consultas(request):
-    return render(request, 'consultas.html')
+    users = User.objects.all()
+    return render(request, 'consultas.html', {'users': users})
 
 def capturas(request):
     return render(request, 'capturas.html')
